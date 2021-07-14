4a0310daf4901d990aec8cec95a147f0571e7005
Dark Tornado <dt3141592@naver.com>
2021-07-14 16:29:04 +0900

    임시 UI

--- java/com/darktornado/ujsspam/MainActivity.java
+++ java/com/darktornado/ujsspam/MainActivity.java
@@ -2,17 +2,38 @@ package com.darktornado.ujsspam;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.Switch;
 
 public class MainActivity extends Activity {
+
+    public static boolean auto = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         LinearLayout layout = new LinearLayout(this);
         layout.setOrientation(1);
-        layout.addView(new EditText(this));
-        setContentView(layout);
+
+        Switch auto = new Switch(this);
+        auto.setText("도배기 실행시 자동 도배");
+        auto.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                MainActivity.auto = isChecked;
+            }
+        });
+        layout.addView(auto);
+
+        EditText txt = new EditText(this);
+        txt.setHint("키보드 테스트용 입력란");
+        layout.addView(txt);
+
+        ScrollView scroll = new ScrollView(this);
+        scroll.addView(layout);
+        setContentView(scroll);
     }
 }
