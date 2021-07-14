94c8bbadea0052f72f5900a04ea0406aee03bf14
Dark Tornado <dt3141592@naver.com>
2021-07-14 15:14:55 +0900

    Update MainActivity.java

--- java/com/darktornado/ujsspam/MainActivity.java
+++ java/com/darktornado/ujsspam/MainActivity.java
@@ -2,11 +2,17 @@ package com.darktornado.ujsspam;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        LinearLayout layout = new LinearLayout(this);
+        layout.setOrientation(1);
+        layout.addView(new EditText(this));
+        setContentView(layout);
     }
 }
